1f299d643e1ecb881aa641ae78173c864386ed0a
Sugam Raut <sugamrautbim@gmail.com>
2025-07-13 22:42:16 +0545

    feat:adding the nav to the lanfding page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/rootlayouts";
 import Home from "./pages/homepage"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/custom.scss"
