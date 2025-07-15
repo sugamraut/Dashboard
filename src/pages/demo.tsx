33ef4507c0cabc0da788f8bd5ed97f9e9e50e7b5
sugamraut <sugamrautbim@gmail.com>
2025-07-15 17:55:29 +0545

    feat:try to implement the table

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,98 +1,59 @@
-import React from 'react';
-import {
-  Box,
-  Drawer,
-  List,
-  Divider,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from '@mui/material';
+import * as React from 'react';
+import { DataGrid, type GridColDef } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
+import Home from './homepage'
 
-import image from "../assets/image/company_name.png";
-import HomeIcon from '@mui/icons-material/Home';
-import FolderIcon from '@mui/icons-material/Folder';
-import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import SummarizeIcon from '@mui/icons-material/Summarize';
-import CategoryIcon from '@mui/icons-material/Category';
-import PeopleIcon from '@mui/icons-material/People';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import FolderOpenIcon from '@mui/icons-material/FolderOpen';
-import PublicIcon from '@mui/icons-material/Public';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
-import HelpIcon from '@mui/icons-material/Help';
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (_value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  },
+];
 
-export default function PersistentLeftDrawer() {
-  const anchor: 'left' = 'left';
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
-  const menuItems = [
-    { text: 'Dashboard', icon: <HomeIcon /> },
-    { text: 'Applications', icon: <FolderIcon /> },
-    { text: 'Forms', icon: <TextSnippetIcon /> },
-    { text: 'Reports', icon: <SummarizeIcon /> },
-    { text: 'Categories', icon: <CategoryIcon /> },
-    { text: 'Users', icon: <PeopleIcon /> },
-    { text: 'Roles & Access', icon: <AdminPanelSettingsIcon /> },
-    { text: 'File Manager', icon: <FolderOpenIcon /> },
-    { text: 'Web Portal', icon: <PublicIcon /> },
-    { text: 'Settings', icon: <SettingsIcon /> },
-  ];
+const paginationModel = { page: 0, pageSize: 5 };
 
-  const bottomItems = [
-    { text: 'Logout', icon: <LogoutIcon /> },
-    { text: 'Help', icon: <HelpIcon /> },
-  ];
-
-  const list = (
-    <Box
-      sx={{
-        width: 250,
-        height: '100%',
-        background: 'linear-gradient(to bottom, #003366, #8b0000)',
-        color: 'white',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Box>
-        <Box sx={{ textAlign: 'center', p: 3, borderBottom: '1px solid rgba(255,255,255,0.2)' }}>
-          <img src={image} alt="Sunlife Logo" width="120" />
-        </Box>
-
-        <List>
-          {menuItems.map(({ text, icon }) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon sx={{ color: 'white' }}>{icon}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-
-      <Box>
-        <Divider sx={{ borderColor: 'rgba(255,255,255,0.2)' }} />
-        <List>
-          {bottomItems.map(({ text, icon }) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon sx={{ color: 'white' }}>{icon}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </Box>
-  );
-
+export default function DataTable() {
   return (
-    <Drawer anchor={anchor} open={true} variant="persistent">
-      {list}
-    </Drawer>
+    <>
+    <Home/>
+     
+    <Paper sx={{ height: 400, width: '100%', }}>
+   
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+      />
+    </Paper>
+    </>
+   
   );
 }
