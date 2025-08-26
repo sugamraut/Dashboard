661c54b2fbc89e8b701ac3f1e4322f11531fcdce
sugamraut <sugamrautbim@gmail.com>
2025-08-26 17:41:01 +0545

    fix:removin the test package that is mui/lab

--- src/pages/loader.tsx
+++ src/pages/loader.tsx
@@ -1,48 +1,31 @@
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import LoadingButton from '@mui/lab/LoadingButton';
-import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function LoadingButtons() {
   return (
-    <Dialog
-      open={true}
-      fullWidth
-      maxWidth="sm"
-    >
+    <Dialog open={true} fullWidth maxWidth="sm">
       <DialogContent>
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '300px',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "300px",
           }}
         >
           <Stack spacing={2}>
             <Stack direction="row" spacing={2}>
-              <LoadingButton
-                variant="outlined"
-                loading
-                sx={{
-                  height: 340,
-                  width: 600,
-                }}
-                loadingIndicator={
-                  <CircularProgress
-                    size={60}
-                    sx={{ color: 'primary.main' }}
-                  />
-                }
-              >
-                Submit
-              </LoadingButton>
+              <CircularProgress />
             </Stack>
           </Stack>
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
         </Box>
       </DialogContent>
     </Dialog>
   );
-}   
+}
