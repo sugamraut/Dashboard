5d97aa43ed071c8deefbe9f0d0e5c15eeb2c60a9
sugamraut <sugamrautbim@gmail.com>
2025-08-29 12:06:56 +0545

    fix :the edit the loader

--- src/pages/loader.tsx
+++ src/pages/loader.tsx
@@ -1,41 +1,20 @@
-// import Dialog from "@mui/material/Dialog";
-// import DialogContent from "@mui/material/DialogContent";
-// import Stack from "@mui/material/Stack";
-// import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
-import { Modal } from "@mui/material";
-import React from "react";
+import { Box } from "@mui/material";
 
 export default function Loading() {
-  const [open, setOpen] = React.useState(false);
-  // const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   return (
-    // <Dialog open={true} fullWidth maxWidth="sm">
-    //   <DialogContent>
-    //     <Box
-    //       sx={{
-    //         display: "flex",
-    //         justifyContent: "center",
-    //         alignItems: "center",
-    //         height: "300px",
-    //       }}
-    //     >
-    //       <Stack spacing={2}>
-    //         <Stack direction="row" spacing={2}>
-    //           <CircularProgress />
-    //         </Stack>
-    //       </Stack>
-    //     </Box>
-    //   </DialogContent>
-    // </Dialog>
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+
+        backgroundColor: "#f1f5f8",
+      }}
     >
-      <CircularProgress />
-    </Modal>
+      <CircularProgress className="modal-loading-indicator" />
+    </Box>
   );
 }
